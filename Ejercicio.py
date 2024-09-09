7a0fc5f84ae75d995991df3e10cf8bdee81dcd8a
Max Rada <xam900mr@gmail.com>
2024-09-08 20:29:19 -0600

    Flask completo

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,34 +1,67 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
 
 db = mysql.connector.connect(
     host="localhost",
-    user="root",   
+    user="root",
     password="ramm160799",
     database="prueba1"
 )
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        sexo = request.form['sexo']
-        edad = request.form['edad']
+    cursor = db.cursor()
+    
+    cursor.execute("SELECT * FROM alumnos")
+    alumnos = cursor.fetchall()
 
-        cursor = db.cursor()
+    return render_template('Diseno.html', alumnos=alumnos, alumno_edit=None)
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    cursor = db.cursor()
+    alumno_id = request.form['id']
+    nombre = request.form['nombre']
+    sexo = request.form['sexo']
+    edad = request.form['edad']
+
+    if alumno_id:
+        sql = "UPDATE alumnos SET nombre = %s, sexo = %s, edad = %s WHERE id = %s"
+        val = (nombre, sexo, edad, alumno_id)
+        
+    else:  
         sql = "INSERT INTO alumnos (nombre, sexo, edad) VALUES (%s, %s, %s)"
         val = (nombre, sexo, edad)
 
-        cursor.execute(sql, val)
-        db.commit()
+    cursor.execute(sql, val)
+    db.commit()
 
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/<int:id>', methods=['GET'])
+def edit(id):
     cursor = db.cursor()
-    cursor.execute("SELECT nombre, sexo, edad FROM alumnos")
+    cursor.execute("SELECT * FROM alumnos WHERE id = %s", (id,))
+    alumno = cursor.fetchone()
+
+    cursor.execute("SELECT * FROM alumnos")
     alumnos = cursor.fetchall()
 
-    return render_template('Diseno.html', alumnos=alumnos)
+    return render_template('Diseno.html', alumnos=alumnos, alumno_edit=alumno)
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    cursor = db.cursor()
+    sql = "DELETE FROM alumnos WHERE id = %s"
+    cursor.execute(sql, (id,))
+    db.commit()
+
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
